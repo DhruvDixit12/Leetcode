8159daab939a296ccb4423b6cb45782a46a1da74
DhruvDixit12 <143922932+DhruvDixit12@users.noreply.github.com>
2025-09-03 16:42:27 +0530

    Two sum.cpp

---
+++ Two sum.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen; // value -> index
+        for (int i = 0; i < (int)nums.size(); ++i) {
+            int need = target - nums[i];
+            auto it = seen.find(need);
+            if (it != seen.end()) return {it->second, i};
+            seen[nums[i]] = i;
+        }
+        return {}; // problem guarantees a solution
+    }
+};
